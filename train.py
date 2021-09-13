8bb23b69989b0c956c42697268644dd442c0ecfd
Ted Lu <49829569+Sax-Ted@users.noreply.github.com>
2021-09-13 19:50:15 +0800

    Update train.py

--- train.py
+++ train.py
@@ -245,7 +245,7 @@ def main():
 
     # Data loading code
     img_ids = glob(os.path.join('inputs', config['dataset'], 'images', '*' + config['img_ext']))
-    img_ids = [p.split(".")[0] for p in img_ids]
+    img_ids = [os.path.splitext(os.path.basename(p))[0] for p in img_ids]
 
     train_img_ids, val_img_ids = train_test_split(img_ids, test_size=0.2, random_state=41)
 
